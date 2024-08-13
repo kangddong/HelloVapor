d173b782345835ef33692b20493e7404392b0a1c
kangddong <rkdehddud96@naver.com>
2024-08-14 02:12:16 +0900

    image Upload API

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,30 @@
 import Vapor
+import FluentKit
 
 func routes(_ app: Application) throws {
-    app.get { req async in
-        "It works!"
-    }
-
-    app.get("hello") { req async -> String in
-        "Hello, world!"
+    let v1Routes = app.grouped("v1")
+    v1Routes.post("images", "upload") { req -> EventLoopFuture<Response> in
+        struct ImageUpload: Content {
+            var file: File
+        }
+        
+        do {
+            let imageUpload = try req.content.decode(ImageUpload.self)
+            let file = imageUpload.file
+            let savePath = "Public/images/\(file.filename)"
+            print("savePath: \(savePath)")
+            
+            return req.fileio.writeFile(file.data, at: savePath)
+                .map {
+                    let response = ["status": "success upload", "fileName": file.filename]
+                    return Response(status: .ok, body: .init(string: response.description))
+                }
+        } catch {
+            let response = ["status": "error", "message": "Failed to decode request"]
+            return req.eventLoop.makeSucceededFuture(
+                Response(status: .ok, body: .init(string: response.description))
+            )
+        }
     }
 }
+

7216e800361cd164b72780272fdb955962da94cb
kangddong <rkdehddud96@naver.com>
2024-08-16 12:34:50 +0900

    [ADD] Image Controller

---
+++ Sources/App/Controllers/ImageUploadController.swift
@@ -0,0 +1,14 @@
+//
+//  ImageUploadController.swift
+//
+//
+//  Created by 강동영 on 8/16/24.
+//
+
+import Vapor
+
+struct ImageUploadController: RouteCollection {
+    func boot(routes: RoutesBuilder) throws {
+        
+    }
+}

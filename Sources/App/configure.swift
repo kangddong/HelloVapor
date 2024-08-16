5722b174712c42fd5fd9cc236c357abc217846bb
kangddong <rkdehddud96@naver.com>
2024-08-16 09:15:35 +0900

    주석 추가

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -5,6 +5,7 @@ public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     app.routes.defaultMaxBodySize = "10mb"
+    // Device에서 사용하고 싶을시에
 //    let homeIP = "111.22.3.44" // ifconfig | grep inet
 //    app.http.server.configuration.hostname = homeIP
     
